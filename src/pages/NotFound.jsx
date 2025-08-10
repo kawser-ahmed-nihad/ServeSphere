a52e6043dccf48e0324600bd9b3b24cff9fc2041
kawser11223 <148429350+kawser11223@users.noreply.github.com>
2025-08-10 18:06:56 +0600

    404 page upadate

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router';
 
 import errorPng from '../assets/404.png'
 import { Helmet } from 'react-helmet';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 const NotFound = () => {
     return (
@@ -12,11 +13,11 @@ const NotFound = () => {
                 <title>ServeSphere || Page not Found</title>
             </Helmet>
 
-            <div className="min-h-screen  dark:bg-gray-800   dark:text-white flex flex-col justify-center items-center bg-white px-4 text-center">
-                <img
-                    src={errorPng}
-                    alt="404 illustration"
-                    className="w-full max-w-md mx-auto mb-8"
+            <div className=" dark:bg-gray-800 mt-24 md:mt-4 dark:text-white flex flex-col justify-center items-center bg-white px-4 text-center">
+                <DotLottieReact
+                    src="https://lottie.host/ed6b01d7-e123-47e4-90fa-2c172df78845/lQH0xR3mag.lottie"
+                    loop
+                    autoplay
                 />
 
                 <h1 className="text-4xl md:text-5xl font-bold text-gray-800 mb-4">
